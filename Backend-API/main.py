2e72eca26acc2b3b7f42e621c0ff64aa5882d593
MatteoOlarte <115902090+MatteoOlarte@users.noreply.github.com>
2024-07-07 17:06:37 -0500

    docs: document and optimize merge_pdf endpoint with dependency injection
    - Documented the merge_pdf endpoint, providing clear usage instructions and examples.
    - Optimized the merge_pdf endpoint by implementing dependency injection, enhancing code modularity and testability.
    - Refactored related code to accommodate the new dependency injection pattern.
    - Updated relevant tests to reflect the changes and ensure continued functionality.

--- Backend-API/main.py
+++ Backend-API/main.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 
 from .routers import (pdf_files)
 
-app = FastAPI()
+app = FastAPI(title='iHate PyPDF')
 app.include_router(pdf_files.router)
--- Backend-API/routers/pdf_files.py
+++ Backend-API/routers/pdf_files.py
@@ -4,18 +4,28 @@ from typing import Annotated
 import pypdf
 from fastapi import (
     APIRouter,
-    File,
-    UploadFile,
     HTTPException,
-    status
+    UploadFile,
+    status,
+    Depends,
+    File,
+    Query
 )
-from starlette.responses import StreamingResponse
+from fastapi.responses import StreamingResponse
 from pypdf.errors import PyPdfError
 
+MAX_FILE_SIZE = 200
 router = APIRouter(prefix='/pdf_files', tags=['PDF Utilities'])
+non_pdf_error = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail='file is not a PDF',
+    headers={
+        'X-Error': 'NonPDFFile'
+    }
+)
 
 
-def to_bytes(pdf_writer: pypdf.PdfWriter) -> io.BytesIO:
+def _to_bytes(pdf_writer: pypdf.PdfWriter) -> io.BytesIO:
     buffer = io.BytesIO()
     print('writing')
     pdf_writer.write(buffer)
@@ -25,8 +35,33 @@ def to_bytes(pdf_writer: pypdf.PdfWriter) -> io.BytesIO:
     return buffer
 
 
-@router.post('/merge_pdf')
-async def merge_pdf(upload_files: Annotated[list[UploadFile], File(...)]) -> StreamingResponse:
+def _validate_size_or_raise(upload_files: Annotated[list[UploadFile], File(...)]) -> None:
+    file_size = sum(file.size for file in upload_files) / 1_000_000
+    if file_size > MAX_FILE_SIZE:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'file size is larger than {MAX_FILE_SIZE:<1}mb limit',
+            headers={
+                'X-Error': 'FileTooLarge'
+            }
+        )
+
+
+def _upload_files(files: Annotated[list[UploadFile], File(...)]) -> list[UploadFile]:
+    _validate_size_or_raise(files)
+    return files
+
+
+@router.post('/merge')
+async def merge_pdf(
+        upload_files: Annotated[list[UploadFile], Depends(_upload_files)],
+        strict: Annotated[bool, Query(..., description='strict mode')] = False
+) -> StreamingResponse:
+    """
+    Merge multiple PDF files into a single PDF file.
+    - **strict**: If false then non-PDF files will be ignored. Otherwise, an error will be raised.
+    - **upload_files**: Files to be merged.
+    """
     if len(upload_files) < 2:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -35,18 +70,8 @@ async def merge_pdf(upload_files: Annotated[list[UploadFile], File(...)]) -> Str
                 'X-Error': 'InsufficientFiles'
             }
         )
-    file_size = sum(file.size for file in upload_files) / 1_000_000
     writer = pypdf.PdfWriter()
     headers = {}
-
-    if file_size > 200:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='file size is too large',
-            headers={
-                'X-Error': 'FileTooLarge'
-            }
-        )
 
     for file in upload_files:
         try:
@@ -54,13 +79,15 @@ async def merge_pdf(upload_files: Annotated[list[UploadFile], File(...)]) -> Str
             reader = pypdf.PdfReader(file.file)
             writer.append(reader)
         except PyPdfError:
+            if strict:
+                raise non_pdf_error
             continue
     headers.update({
         'Content-Type': 'application/pdf',
         'Content-Disposition': f'attachment; filename=merged_pdf.pdf'
     })
     return StreamingResponse(
-        content=iter([to_bytes(writer).getvalue()]),
+        content=iter([_to_bytes(writer).getvalue()]),
         media_type='application/pdf',
         headers=headers
     )
