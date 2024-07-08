9b0b727c613d911885fec0e322647418e2152dcb
MatteoOlarte <115902090+MatteoOlarte@users.noreply.github.com>
2024-07-07 21:18:12 -0500

    feat: encrypt and decrypt pdf files endpoints

--- Backend-API/routers/pdf_files.py
+++ Backend-API/routers/pdf_files.py
@@ -1,4 +1,5 @@
 import io
+import zipfile
 from typing import Annotated
 
 import pypdf
@@ -27,11 +28,8 @@ non_pdf_error = HTTPException(
 
 def _to_bytes(pdf_writer: pypdf.PdfWriter) -> io.BytesIO:
     buffer = io.BytesIO()
-    print('writing')
     pdf_writer.write(buffer)
-    print('done')
     buffer.seek(0)
-    print('returning')
     return buffer
 
 
@@ -47,14 +45,30 @@ def _validate_size_or_raise(upload_files: Annotated[list[UploadFile], File(...)]
         )
 
 
-def _upload_files(files: Annotated[list[UploadFile], File(...)]) -> list[UploadFile]:
+def file_uploads(files: Annotated[list[UploadFile], File(...)]) -> list[UploadFile]:
     _validate_size_or_raise(files)
     return files
 
 
+def file_upload(file: Annotated[UploadFile, File(...)]) -> UploadFile:
+    _validate_size_or_raise([file])
+    return file
+
+
+def zip_files(files: dict[str, pypdf.PdfWriter]):
+    zip_io = io.BytesIO()
+
+    with zipfile.ZipFile(zip_io, 'w') as zip_file:
+        for name, i in files.items():
+            file_io = _to_bytes(i)
+            zip_file.writestr(f'{name}', file_io.getvalue())
+
+    return zip_io
+
+
 @router.post('/merge')
 async def merge_pdf(
-        upload_files: Annotated[list[UploadFile], Depends(_upload_files)],
+        upload_files: Annotated[list[UploadFile], Depends(file_uploads)],
         strict: Annotated[bool, Query(..., description='strict mode')] = False
 ) -> StreamingResponse:
     """
@@ -88,6 +102,87 @@ async def merge_pdf(
     })
     return StreamingResponse(
         content=iter([_to_bytes(writer).getvalue()]),
+        media_type='application/pdf',
+        headers=headers
+    )
+
+
+@router.post('/protect')
+async def protect_pdf(
+        uploaded_files: Annotated[list[UploadFile], Depends(file_uploads)],
+        user_password: Annotated[str, Query(..., description='password to protect the PDF file')],
+        strict: Annotated[bool, Query(..., description='strict mode')] = False
+):
+    """
+    Protect a PDF file with a password.
+    - **uploaded_files**: Files to be protected.
+    - **password**: Password to protect the PDF file.
+    """
+    pdf_files: dict[str, pypdf.PdfWriter] = {}
+    zip_file = ...
+
+    for file in uploaded_files:
+        await file.seek(0)
+
+        try:
+            reader = pypdf.PdfReader(file.file)
+            writer = pypdf.PdfWriter()
+            writer.append(reader)
+            writer.encrypt(user_password, None, True)
+            pdf_files.update({f'{file.filename}': writer})
+        except PyPdfError:
+            if strict:
+                raise non_pdf_error
+            continue
+
+    zip_file = zip_files(pdf_files)
+    headers = {
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': f'attachment; filename=protected_pdf.zip'
+    }
+    return StreamingResponse(
+        content=iter([zip_file.getvalue()]),
+        media_type='application/zip',
+        headers=headers
+    )
+
+
+@router.post('/unlock')
+async def unlock_pdf(
+        upload_file: Annotated[UploadFile, Depends(file_upload)],
+        password: Annotated[str, Query(..., description='password to unlock the PDF file')]
+):
+    """
+    Unlock a PDF file with a password.
+    - **upload_file**: File to be unlocked.
+    - **password**: Password to unlock the PDF file.
+    """
+    file_name = upload_file.filename
+    file_io = ...
+    headers = ...
+
+    try:
+        reader = pypdf.PdfReader(upload_file.file)
+        writer = ...
+
+        if reader.is_encrypted:
+            reader.decrypt(password)
+        writer = pypdf.PdfWriter(clone_from=reader)
+        file_io = _to_bytes(writer)
+    except PyPdfError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='invalid password',
+            headers={
+                'X-Error': 'InvalidPDFPassword'
+            }
+        )
+    headers = {
+        'Content-Type': 'application/pdf',
+        f'Content-Disposition': f'attachment; filename={file_name}'
+    }
+    return StreamingResponse(
+        content=iter([file_io.getvalue()]),
         media_type='application/pdf',
         headers=headers
     )
